b92975d45436002c36372b17c0a1270cc57f8203
Matt Lilley <matt.lilley@securitease.com>
2025-07-28 19:03:57 +0100

    Update demo to handle streaming

--- tts.py
+++ tts.py
@@ -1,6 +1,14 @@
 from gpt_sovits.api import GPTSoVITS
+import soundfile as sf
+import numpy as np
+import time
 
 tts = GPTSoVITS()
-wav = tts.generate(target_text="No body of water is safe without a lifeguard; that is true. That includes bathtubs.",
-                   ref_audio_path="ref.wav",
-                   output_path="sovits.wav")
+
+t0 = time.time()
+chunks = []
+for chunk in tts.generate(target_text="No body of water is safe without a lifeguard; that is true. That includes bathtubs.",
+                          ref_audio_path="ref.wav"):
+    chunks.append(chunk)
+print(f"Total time: {time.time() - t0}")
+sf.write("sovits-stream.wav", np.concatenate(chunks), samplerate=44100)
